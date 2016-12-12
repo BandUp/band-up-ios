1b8ec399daadafdf203676af01dd90eec5c5e39b
Bergþór Þrastarson <bergthor13@ru.is>
2016-12-12 15:34:21 +0000

    Adding Password Reset

---
+++ band-up-ios/PasswordResetViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PasswordResetViewController.swift
+//  band-up-ios
+//
+//  Created by Bergþór on 12.12.2016.
+//  Copyright © 2016 Bad Melody. All rights reserved.
+//
+
+import UIKit
+
+class PasswordResetViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
+
