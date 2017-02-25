c50e87f1464950012bd819dc596302cec5183635
Bergþór Þrastarson <bergthor13@ru.is>
2017-02-25 06:27:55 +0000

    Adding matches to widget and iMessage Stickers

--- MatchesWidget/TodayViewController.swift
+++ MatchesWidget/TodayViewController.swift
@@ -32,3 +32,35 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
     
 }
+
+extension TodayViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 4
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+		let image = cell.viewWithTag(1) as! UIImageView
+		let name = cell.viewWithTag(2) as! UILabel
+		let message = cell.viewWithTag(3) as! UILabel
+		image.clipsToBounds = true
+		image.layer.cornerRadius = image.frame.width/2
+		name.text = "Username \(indexPath.row+1)"
+		if indexPath.row == 0 {
+			message.isHidden = false
+		} else {
+			message.isHidden = true
+		}
+
+		return cell
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+		let frameWidth: CGFloat = collectionView.frame.width
+		let itemWidth: CGFloat = (frameWidth / CGFloat(4.0))
+
+
+		return CGSize(width: itemWidth, height: CGFloat(110))
+	}
+}
